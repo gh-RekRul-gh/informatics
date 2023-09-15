0910a125b217def8a69ef816fdc8b1140a8f7505
gh-RekRul-gh <rerusya18@mail.ru>
2023-09-15 22:18:39 +0300

    sd(k)nf changed

--- sd(k)nf.py
+++ sd(k)nf.py
@@ -1,35 +1,35 @@
 from math import log2
 
 
-def main(s, n):
+def main(currentSetOfNumbers, requiredNumberOfNumbers):
     global sdnf, sknf, functionBin
-    if len(s) < n:
+    if len(currentSetOfNumbers) < requiredNumberOfNumbers:
         for i in range(2):
-            main(s + str(i), n)
+            main(currentSetOfNumbers + str(i), requiredNumberOfNumbers)
         return
 
     else:
-        indexOfCurrentFunctionBit = int(s, 2)
+        indexOfCurrentFunctionBit = int(currentSetOfNumbers, 2)
         if functionBin[indexOfCurrentFunctionBit] == '0':
             if len(sknf) != 0:
                 sknf += ' * '
             sknf += '('
-            for i in range(n):
-                if s[i] == '1':
+            for i in range(requiredNumberOfNumbers):
+                if currentSetOfNumbers[i] == '1':
                     sknf += '|'
                 sknf += chr(97 + i)
-                if i != n - 1:
+                if i != requiredNumberOfNumbers - 1:
                     sknf += '+'
             sknf += ')'
         else:
             if len(sdnf) != 0:
                 sdnf += ' + '
             sdnf += '('
-            for i in range(n):
-                if s[i] == '0':
+            for i in range(requiredNumberOfNumbers):
+                if currentSetOfNumbers[i] == '0':
                     sdnf += '|'
                 sdnf += chr(97 + i)
-                if i != n - 1:
+                if i != requiredNumberOfNumbers - 1:
                     sdnf += '*'
             sdnf += ')'
 
