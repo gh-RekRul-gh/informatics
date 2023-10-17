39cd2ffaa670031a5a525507ad60486120175dfd
gh-RekRul-gh <rerusya18@mail.ru>
2023-10-17 13:11:36 +0300

    truth_table edited

--- truth_table.py
+++ truth_table.py
@@ -2,16 +2,20 @@ from math import log2
 
 
 def truthTable(currentSetOfNumbers):
+
     global n, functionBin
+
     if len(currentSetOfNumbers) == 0:
         for i in range(n):
             print(chr(97 + i), end=' | ')
         print('F')
         print('-' * (4 * n + 2))
+
     if len(currentSetOfNumbers) < n:
         for i in range(2):
             truthTable(currentSetOfNumbers + str(i))
         return
+
     indexOfCurrentBit = int(currentSetOfNumbers, 2)
     for elem in currentSetOfNumbers:
         print(elem, end=' | ')
